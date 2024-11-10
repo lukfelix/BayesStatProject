1709f9bd532ca73ce672edfcea9f6f1100f9b46a
lukfelix <99lukas.felix@gmail.com>
2024-11-10 22:47:39 +0100

    refactoring mcmc procedure

--- analysis.py
+++ analysis.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import batman
-import emcee
 import corner
 import pathlib
 import os
@@ -80,7 +79,7 @@ fig, ax = plot_single_light_curve(flux_data, time_data, all_errors_dict, plt_siz
 
 # Save the light curve plot
 light_curve_plot_name = "outputs/plots/light_curve_plot_sim"
-#TODO change naming above to match all the model parameters (not very important)
+#TODO change naming above to match all the model parameters (not very important, only relevant if we actually change them)
 if not os.path.exists(light_curve_plot_name):
     fig.savefig(light_curve_plot_name, dpi=300)
 
@@ -100,26 +99,7 @@ error_data = all_errors_dict['1000 ppm']
 ######################### FITTING DATA TO A MODEL ##############################
 ################################################################################
 
-# Our goal is to estimate the parameters of the model that best fit the data.
-# The parameters we want to esimate are (for the moment): 'ps', 'u1' and 'u2'. 
-
-# 1. ** Define the Model **:                Set up the physical model that you will fit to the data. This involves specifying the transit light curve model with the parameters you wish to estimate. We already did this through the batman model.
-# 2. ** Choose Priors **:                   Define prior distributions for each parameter. Priors represent your beliefs about the parameters before seeing the data.
-# 3. ** Set Up the Likelihood Function **:  This function evaluates how likely the observed data is, given a set of parameters. 
-# 4. ** Initialize the MCMC Sampler **:     Choose an MCMC algorithm (e.g., Metropolis-Hastings, Gibbs sampling, or Hamiltonian Monte Carlo) and set the initial positions of the chains, typically starting from different points to cover the parameter space well.
-# 5. ** Run the MCMC Sampler **:            Execute the chains to explore the parameter space. The sampler will generate a sequence of samples for each parameter.
-# 6. ** Analyze the Results **:             After the MCMC run, analyze the chains for convergence, discard the burn-in phase, and use the remaining samples to compute the posterior distributions and statistics (mean, median, confidence intervals) for each parameter.
-
-# TODO: Check that everything is correct --> play with the priors a bit, see what happens
-
-######################## Step 1 - Define the model #############################
-
-# DONE (see above), here just as recap:
-model = model
-params = params
-
-######################### Step 2 - Choose Priors ###############################
-
+# choose Priors
 # convention:
 # [uniform, lower bound, upper bound]
 # [gauss, mean, sigma]
@@ -136,114 +116,23 @@ param_priors = {
     # 'v':         ['gauss', 0, 1e3],    # allow for slope !=0 in time (dy/dt), needs a gaussian prior to converge consistently
     }
 
-#################### Step 3 - Set Up the Likelihood Function #####################
-
-# p(D|theta)
-def log_likelihood(theta, t, y, yerr):
-    model_prediction = full_model(theta, params, model)
-    return -0.5 * np.sum(((y - model_prediction) / yerr) ** 2)
-
-# p(theta)
-def log_prior(theta):
-    """
-    evaluate priors for given paramter theta, looks unusual due to dictionary structure of priors above
-    """
-    lp = 0.
-    for val, prior in zip(theta, param_priors):
-        # print(val, prior)
-        if param_priors[prior][0] == 'uni':
-            ret = log_uni(val, param_priors[prior][1], param_priors[prior][2])
-        elif param_priors[prior][0]=='gauss':
-            ret = log_gauss(val, param_priors[prior][1], param_priors[prior][2])
-        else:
-            print('Error: prior-distribution "{}" not accepted by current code.'.format(param_priors[prior][0]))
-            return None
-        if ret == -np.inf:# or ret < 1e-20:
-            # print('inf')
-            return -np.inf
-        else:
-            lp += ret
-    return lp
-
-# Define the final probability function as likelihood * prior ('+' due to log).
-# p(theta|D) = p(D|theta) * p(theta)
-def log_posterior(theta, t, y, yerr):
-    lp = log_prior(theta)
-    if not np.isfinite(lp):
-        return -np.inf
-    # print('not inf')
-    return lp + log_likelihood(theta, t, y, yerr)
-
-#%%
-##################### Step 4 - Initialize the MCMC Sampler #######################
-
 # MCMC parameters
-# TODO adapt these to the specific problem
-ndim            =   len(param_priors)
-nwalkers        =   32
-nsteps          =   10000
-burn_in_frac    =   0.4
-
-# Initialize the walkers
-pos = np.zeros((nwalkers, ndim))
-for i, param in enumerate(param_priors):
-    if param_priors[param][0]=='uni':
-        pos[:, i] = np.random.uniform(param_priors[param][1], param_priors[param][2], nwalkers)
-    elif param_priors[param][0]=='gauss':
-        pos[:, i] = np.random.normal(param_priors[param][1], param_priors[param][2], nwalkers)
-    else:
-        print('prior not recognized')
-        break
-
-# Initialize the sampler (at the moment time_data is not being used, see "model_functions.py/full_model()" )
-sampler = emcee.EnsembleSampler(nwalkers, ndim, log_posterior, args=(time_data, flux_data, error_data))
+# TODO: just a working selection right now, could probably be improved
+mcmc_params = {
+    'ndim'        :len(param_priors),
+    'nwalkers'    :32,
+    'nsteps'      :10000,
+    'burn_in_frac':0.4,
+}
 
 #%%
-####################### Step 5 - Run the MCMC Sampler ############################
 
-# Run the sampler
-sampler.run_mcmc(pos, nsteps, progress=True)
+posterior_samples = run_mcmc(time_data, flux_data, error_data, model,
+                            params, param_priors, mcmc_params,
+                            transform=False)
 
-# Get the samples
-samples = sampler.get_chain(discard=int(burn_in_frac * nsteps))     # discard the burn-in phase
-
-# TODO: save samples in some format for more flexible plotting and post-processing
-
-#%%
-####################### Step 6 - Analyze the Results ############################
-
-# Flatten the samples (remove the walkers)
-flattened_samples = samples.reshape(-1, len(param_priors))  # flatten the samples for plotting
-
-# print(flattened_samples.shape)
-
-#%%
 # Plot the corner plot
-
-fig = corner.corner(
-
-    flattened_samples, 
-    title_fmt='.5f',
-    bins=50,
-    show_titles=True,
-    labels=[r"$P_S$", r"$u_1$", r"$u_2$"], 
-    truths=[truths['ps'], truths['u'][0], truths['u'][1]],
-    plot_density=True,
-    plot_datapoints=True,
-    fill_contours=False,
-    smooth=True,
-    levels=(0.6827, 0.90, 0.9545),              # shows the 1, 1.5 and 2 sigma contours in the 2D plots
-    quantiles=[0.16, 0.5, 0.84],                # shows the 1 sigma interval in the 1D plots
-    title_kwargs={"fontsize": 10},
-    truth_color='cornflowerblue',
-
-)
-#%%
-# Save the corner plot
-corner_plot_name = "outputs/plots/corner_plot_%.0fparameters_%.0fppm_no_linear_model" % (len(param_priors), all_errors_dict['1000 ppm'][0]*1e6)
-
-if not os.path.exists(corner_plot_name):
-    fig.savefig(corner_plot_name, dpi=300)
+create_corner_plot(posterior_samples, truths, all_errors_dict['1000 ppm'][0]*1e6, transform=False)
 
 #%%
 
--- mcmc_functions.py
+++ mcmc_functions.py
@@ -1,4 +1,9 @@
 import numpy as np
+import emcee
+import corner
+import os
+
+from model_functions import full_model
 
 # define some necessary math for priors functions
 def log_uni(x, a, b):
@@ -9,3 +14,116 @@ def log_uni(x, a, b):
 def log_gauss(x, mean, sigma):
     return -np.log(np.sqrt(2*np.pi)*sigma) - (x-mean)**2 / (2*sigma**2)
 
+# p(D|theta)
+def log_likelihood(theta, t, y, yerr, params, model, transform):
+    model_prediction = full_model(theta, params, model, transform)
+    return -0.5 * np.sum(((y - model_prediction) / yerr) ** 2)
+
+# p(theta)
+def log_prior(theta, priors):
+    """
+    evaluate priors for given paramter theta, looks unusual due to dictionary structure of priors
+    """
+    lp = 0.
+    for val, prior in zip(theta, priors):
+        # print(val, prior)
+        if priors[prior][0] == 'uni':
+            ret = log_uni(val, priors[prior][1], priors[prior][2])
+        elif priors[prior][0]=='gauss':
+            ret = log_gauss(val, priors[prior][1], priors[prior][2])
+        else:
+            print('Error: prior-distribution "{}" not accepted by current code.'.format(priors[prior][0]))
+            return None
+        if ret == -np.inf:# or ret < 1e-20:
+            # print('inf')
+            return -np.inf
+        else:
+            lp += ret
+    return lp
+
+# Define the final probability function as likelihood * prior ('+' due to log).
+# p(theta|D) = p(D|theta) * p(theta)
+def log_posterior(theta, t, y, yerr, params, model, priors, transform):
+    lp = log_prior(theta, priors)
+    if not np.isfinite(lp):
+        return -np.inf
+    # print('not inf')
+    return lp + log_likelihood(theta, t, y, yerr, params, model, transform)
+
+def run_mcmc(time_data, flux_data, error_data, model,
+             model_params, priors, mcmc,
+             transform=False):
+    """
+    In this function we take all the necessary inputs for running the mcmc provided in the analysis.py file.
+    From there we initialize the mcmc-run and execute it, resulting in posterior samples that are returned.
+    time_data       :   array containing the time values associated with each flux datapoint
+    flux_data       :   array of flux values as simulated by the lightcurve modeling
+    error_data      :   array of errors associated with each simulated flux value
+    model           :   batman model instance (has to be initiated with the same time_data array)
+    model_params    :   batman param instance (needs to already contain the fixed parameter values)
+    priors          :   dict of planet radii and limb-drakening priors, can either be gaussian or uniform atm
+    mcmc            :   dict of mcmc parameters ( 'ndim', 'nwalkers', 'nsteps', 'burn_in_frac')
+    transform=False :   whether or not the limb-darkening is analysed in 
+                            the transformed Kipping parameterization (True) 
+                            or default quadratic (False)
+
+    Returns         :   flattened posterior samples
+    """
+
+    # Initialize the walkers
+    pos = np.zeros((mcmc['nwalkers'], mcmc['ndim']))
+    for i, param in enumerate(priors):
+        if priors[param][0]=='uni':
+            pos[:, i] = np.random.uniform(priors[param][1], priors[param][2], mcmc['nwalkers'])
+        elif priors[param][0]=='gauss':
+            pos[:, i] = np.random.normal(priors[param][1], priors[param][2], mcmc['nwalkers'])
+        else:
+            print('prior not recognized')
+            break
+
+    # Initialize the sampler (at the moment time_data is not being used, see "model_functions.py/full_model()", but would be necessary if one also wants to fit a linear slope)
+    # args are additional inputs besides theta in the log_posterior calculation
+    sampler = emcee.EnsembleSampler(mcmc['nwalkers'], mcmc['ndim'], log_posterior,
+                                    args=(time_data, flux_data, error_data, model_params, model, priors, transform))
+
+    # Run the sampler
+    sampler.run_mcmc(pos, mcmc['nsteps'], progress=True)
+
+    # Get the samples
+    samples = sampler.get_chain(discard=int(mcmc['burn_in_frac'] * mcmc['nsteps']))     # discard the burn-in phase
+
+    # Flatten the samples (remove the walkers)
+    flattened_samples = samples.reshape(-1, len(priors))  # flatten the samples for plotting
+    
+    # TODO: save samples in some format for more flexible plotting and post-processing
+
+    return flattened_samples
+
+def create_corner_plot(posterior_samples, truths, errval, transform=False):
+    """
+    Plotand save the corner plot of the posterior samples, showing the underlying truth values
+    """
+    fig = corner.corner(
+        posterior_samples, 
+        title_fmt='.5f',
+        bins=50,
+        show_titles=True,
+        labels=[r"$P_S$", r"$u_1$", r"$u_2$"], 
+        truths=[truths['ps'], truths['u'][0], truths['u'][1]],
+        plot_density=True,
+        plot_datapoints=True,
+        fill_contours=False,
+        smooth=True,
+        levels=(0.6827, 0.90, 0.9545),              # shows the 1, 1.5 and 2 sigma contours in the 2D plots
+        quantiles=[0.16, 0.5, 0.84],                # shows the 1 sigma interval in the 1D plots
+        title_kwargs={"fontsize": 10},
+        truth_color='cornflowerblue',
+    )
+    if transform:
+        corner_plot_name = "outputs/plots/corner_plot_kipping_%.0fppm_no_linear_model" % (errval)
+    else:
+        corner_plot_name = "outputs/plots/corner_plot_quadratic_%.0fppm_no_linear_model" % (errval)
+
+    if not os.path.exists(corner_plot_name):
+        fig.savefig(corner_plot_name, dpi=300)
+        return
