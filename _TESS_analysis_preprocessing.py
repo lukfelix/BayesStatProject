ca3321a2e303f65f06c364396b44e3d8823cd879
rgm23 <rgm23@ic.ac.uk>
2024-12-03 14:09:50 +0100

    finished Gaussian Process and Preprocessing of TESS data.

--- _TESS_analysis_preprocessing.py
+++ _TESS_analysis_preprocessing.py
@@ -241,6 +241,10 @@ x_time = time_data_norm_longer_3
 y_flux = flux_data_norm_longer_3
 y_err = flux_err_norm_longer_3
 
+np.save("outputs/data_TESS/NEW_time_data_norm.npy", NEW_time_data_norm)
+np.save("outputs/data_TESS/NEW_flux_data_norm.npy", NEW_flux_data_norm)
+np.save("outputs/data_TESS/NEW_flux_err_norm.npy", NEW_flux_err_norm)
+
 # Plot the results
 plt.figure(figsize=(15, 6))
 plt.plot(x_time, y_flux,'.', label="Observed Flux / GP Fitted FLux", color="blue", markersize=2)
@@ -454,6 +458,12 @@ plt.legend()
 plt.show()
 
 #%%
+
+
+x_time = np.load("outputs/data_TESS/NEW_time_data_norm.npy")
+y_flux = np.load("outputs/data_TESS/NEW_flux_data_norm.npy")
+y_err = np.load("outputs/data_TESS/NEW_flux_err_norm.npy")
+
 # Plot the results
 plt.figure(figsize=(15, 6))
 plt.plot(x_time, y_flux,'.', label="Observed Flux / GP Fitted FLux", color="blue", markersize=2)
@@ -463,256 +473,83 @@ plt.legend()
 plt.show()
 
 
+# time
 final_time_data = x_time
 print(len(final_time_data))
 
+# flux
 final_flux_data = y_flux
 final_flux_data[indices_stellar_flux] = flux_with_gaps/flux_stellar_flux
 
 for i in range (len(transit_indices)):
     final_flux_data[transit_indices[i]] = y_flux[transit_indices[i]] / flux[transit_indices[i]]
 
+# flux_err
+final_flux_err_data = y_err
 
-# # Plot the results
-# plt.figure(figsize=(15, 6))
-# plt.plot(final_time_data, final_flux_data,'.', label="Observed Flux / GP Fitted FLux", color="blue", markersize=2)
-# plt.ylabel("Flux")
-# plt.title("Observed Flux divided by Gaussian Process Flux")
-# plt.legend()
-# plt.show()
+# Plot the results
+plt.figure(figsize=(15, 6))
+plt.plot(final_time_data, final_flux_data, label="Observed Flux / GP Fitted FLux", color="blue")
+plt.ylabel("Flux")
+plt.title("Observed Flux divided by Gaussian Process Flux")
+plt.legend()
+plt.show()
+
+#%%
+
+# Plot the cleaned data, zoom_range=(2826.65, 2826.92)
+fig, ax = plot_single_light_curve_with_zoom(final_time_data, final_flux_data, final_flux_err_data, plt_size=(20, 10),zoom_range=(1.4, 1.65))
 
 
+print(len(np.where(np.isnan(final_flux_data))[0]))
 
-
+print(len(final_time_data))
 # %%
 # time --> 12.75 - 13.4 
 
 
+# FILTER OUT THE FINAL TIME: we do not want.
+
+indices_good_flux = np.where((final_time_data < 12.75) | (final_time_data > 13.4))[0]
 
 
 
 
+print(len(indices_good_flux))
 
+fig, ax = plot_single_light_curve_with_zoom(final_time_data[indices_good_flux], final_flux_data[indices_good_flux], final_flux_err_data[indices_good_flux], plt_size=(20, 10),zoom_range=(1.4, 1.65))
 
+print(np.average(final_flux_data[indices_good_flux]))
 
+#%%
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Create a Gaussian Process fit where we only fit a certain variance around the mean
-
-# kernel_2 = (
-#     1.0 * kernels.ExpSquared(scale=8)  # Smooth variation         # 1.0, 5.0
-# )
-
-# slope_2 = 0
-# mean_func_2 = lambda x: jnp.mean(flux_stellar_flux) + slope * x
-
-# # Define the Gaussian Process model
-# gp_2 = GaussianProcess(kernel_2, extended_time_data_obs, diag=extended_flux_err**2, mean=mean_func_2)
-
-# # Compute the GP conditioned on the data
-# gp_conditioned_2 = gp_2.condition(y=extended_flux_data, X_test=extended_time_data).gp
-
-# mu_2, std_2 = gp_conditioned_2.loc, jnp.sqrt(gp_conditioned_2.variance)
-
-# #%%
-
-# # Plot the results
-# plt.figure(figsize=(15, 6))
-# # plt.plot(extended_time_data_obs, extended_flux_data, ".", label="Extended Data", color="green", markersize=2)
-# # plt.plot(time_stellar_flux, flux_stellar_flux,'.', label="Observed Data", color="blue", markersize=2)
-# # plt.fill_between(extended_time_data, mu_2-std_2, mu_2+std_2, color = "red", alpha=0.5, label="GP Uncertainty")
-# plt.plot(extended_time_data, mu_2,'.', alpha=0.5, label="GP fit", color="red", markersize=3)
-# plt.ylabel("Flux")
-# plt.title("Gaussian Process Regression to Fill Gaps in Stellar Flux Data, DIRECT FIT")
-# plt.legend()
-# plt.show()
-
-
-
+# FINAL DATA
+FINAL_time_data_ = final_time_data[indices_good_flux]
+FINAL_flux_data = final_flux_data[indices_good_flux]
+FINAL_flux_err_data = final_flux_err_data[indices_good_flux]
 
 
 #%%
 
-# TODO:
-# - 1. Find a way to save Gaussian Process data                 (not high priority)
-# - 2. fill the original 4 gaps with the Gaussian Process data
-# - 3. fill in the transits again
-# - 4. Save this data
-# - 5. Run the MCMC analysis on this transit data on only first 1 transit
-# - 6. Run the MCMC analysis on this transit data on all transits including the Periodicity
+########################################################
+
+########################################################
+
+########################################################
 
 
-#%%
+## FINISHED PREPROCESSING - SAVING THE PREPROCESSED TESS DATA - ##
+
+########################################################
+
+########################################################
+
+########################################################
 
 
-# CREATE THE FINAL DATA SET
-# Step 1: Fill the gaps with the Gaussian Process data
-
-# Fill the gaps with the Gaussian Process data, recall indices of the gaps
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-################### Step 2 - Initialize Model Parameters #######################
-# Define & initialize the parameters for the light curve simulation. Based on the parameters of the paper
-
-# TRUE VALUES (those are the parameters we want to estimate with MCMC)
-# most references are from https://exoplanets.nasa.gov/exoplanet_watch_results/hd_209458_b/?utm_source=chatgpt.com
-
-truths = {
-    'ps':0.1268,                     # Ref: ExoFOP-TESS TOI
-    'u1':0,                          # NO REF (assuming 0 is wrong!)
-    'u2':0,                          # NO REF (assuming 0 is wrong!)
-    'ecc':0.01,                      # Ref: Rosenthal et al. 2021
-    't0':0,                          # NO REF (assuming 0 is wrong!)
-}
-
-# FIXED VALUES (those are the parameters we assume to be known)
-fixed_params = {
-    # orbital parameters:
-    'period':3.52474955,             # orbital period (in days):                REF: Kokori et al. 2023
-    'a':8.76,                        # semi-major axis in stellar radii:        REF: Kokori et al. 2023
-    'inc':86.71,                     # orbital inclination in degrees           REF: Kokori et al. 2023
-    'omega':0,                       # longitude of periastron (in degrees)     REF: Rosenthal et al. 2021
-    'limb_dark_model':"quadratic",   # limb-darkening model                     REF: https://arxiv.org/pdf/0802.3764
-    # observation parameters from dataset:
-    'n_points':1000,                 # number of points in the light curve
-    't_min':-0.25,                   # minimum time in days
-    't_max':0.25,                    # maximum time in days
-}
-# Initialize the parameters accordingly, to match the "syntax" of the batman package
-params, t_array = initialize_parameters(truths, fixed_params)
-
-
-#%%
-################ Step 2 - Create Model & Simulate Light Curve ##################
-
-# Initialize the batman model using the parameters from above
-model, time_data, flux_data = initialize_model(params, t_array)    #initializes model for the simulation
-
-# Define the error envelopes for the light curve ranging from 1-1000 ppm & store them in a dictionary. 
-all_errors_dict = {
-    "1 ppm":    (1    / 1e6) * flux_data,       # error envelope for 1 ppm
-    "10 ppm":   (10   / 1e6) * flux_data,       # error envelope for 10 ppm
-    "30 ppm":   (30   / 1e6) * flux_data,       # error envelope for 30 ppm
-    "100 ppm":  (100  / 1e6) * flux_data,       # error envelope for 100 ppm,
-    "300 ppm":  (300  / 1e6) * flux_data,       # error envelope for 300 ppm, 
-    "1000 ppm": (1000 / 1e6) * flux_data,       # error envelope for 1000 ppm
-    }  
-
-save_simdata(time_data, flux_data, all_errors_dict, 'no_ld')
-# the reload below is unnecessary, but here to illustrate how to load the data if necessary later on:
-time_data, flux_data, all_errors_dict = load_simdata_all_errs('no_ld')
-
-
-
-#%%
-##################### Step 3 - Plot & Save Light Curve #######################
-
-# Plot the simulated light curve. This is the light curve we assume to be the true data. The error envelopes represent our "observed" data
-fig, ax = plot_single_light_curve(flux_data, time_data, all_errors_dict, plt_size=(15, 8))
-
-# Dynamically generate the filename with all truths parameters
-output_plot_dir = pathlib.Path("outputs/plots")
-truths_str = get_name_str(truths=truths)
-light_curve_plot_name = output_plot_dir / f"light_curve_plot_sim_{truths_str}.png"
-
-# Save the light curve plot
-if not os.path.exists(light_curve_plot_name):
-    fig.savefig(light_curve_plot_name, dpi=300)
-
-
-#%%
-################################################################################
-######################### Run Quadratic Limb-Drkening ##########################
-################################################################################
-
-# choose Priors
-# convention:
-# [uniform, lower bound, upper bound]
-# [gauss, mean, sigma]
-param_priors = {
-    'ps':        ['uni', 0., 0.5],     # stellar radii
-    'u1':        ['uni', -3., 3],      # limb darkening
-    'u2':        ['uni', -3., 3.],     # limb darkening
-}
-
-# MCMC parameters
-mcmc_params = {
-    'ndim'        :len(param_priors),
-    'nwalkers'    :4*len(param_priors),
-    'nsteps'      :100000,
-    'burn_in_frac':0.6,
-}
-
-# iterate over all errors, creating output plots, may take a while!
-# if you want to save the file, additionally pass the argument save=get_name_str(truths)
-# run_full_routine(truths, params, model, param_priors, mcmc_params, 
-#                  time_data, flux_data, all_errors_dict, 
-#                  transform=False)#, save=get_name_str(truths))
-#%%
-################################################################################
-######################### Run Kipping Limb-Drkening ############################
-################################################################################
-
-# update priors and ground truth to Kipping
-param_priors = {
-    'ps':        ['uni', 0., 0.5],    # stellar radii
-    'u1':        ['uni', 0., 1.],     # limb darkening
-    'u2':        ['uni', 0., 1.],     # limb darkening
-}
-
-truths = {
-    'ps':0.1,                        # planet-to-star radius ratio = planet radius (in units of stellar radii)
-    'u1':0,                          # limb-darkening coefficients: q1 
-    'u2':None,                       # limb-darkening coefficients: q2
-    # TODO: q2 is not actually well defined... need to calculate the limes of 0.5 * u1 / (u1 + u2) for u1 & u2 -> 0
-    # but it diverges (gives different results depending which param you let go to 0 first)
-}
-
-# iterate over all errors, creating output plots, may take a while!
-# if you want to save the file, additionally pass the argument save=get_name_str(truths)
-run_full_routine(truths, params, model, param_priors, mcmc_params, 
-                 time_data, flux_data, all_errors_dict, 
-                 transform=True)#, save=get_name_str(truths))
+# Save the preprocessed data
+np.save("outputs/data_TESS/FINAL_time_data.npy", FINAL_time_data_)
+np.save("outputs/data_TESS/FINAL_flux_data.npy", FINAL_flux_data)
+np.save("outputs/data_TESS/FINAL_flux_err_data.npy", FINAL_flux_err_data)
 
 #%%
